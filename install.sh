56f64a529aad396077efeb9bd4fdc368d2165aa6
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-02 17:18:32 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,130 +2,59 @@
 # -*- ENCODING: UTF-8 -*-
 #inicio
     clear
-    echo ::::::::::::::::::::::::::::::::::
-    echo ::::__script_de_instalacion___::::
-    echo ::::::::::::::::::::::::::::::::::
-    sleep 5
-    clear
-    echo :::::::::::::::
+    echo ::{:::::::::::::::::::::::}::
+    echo ::__script_de_instalacion__::
+    echo ::{:::::::::::::::::::::::}::
+    sleep 1
+    echo ::{:::::::::::::::}::
     echo ::Version:1.3{beta}::
-    echo :::::::::::::::
+    echo ::{:::::::::::::::}::
+    sleep 1
+    echo ::{:::::::::::::::::::::::::::::}::
+    echo ::Te_pedira_la_contraseña_5_veces::
+    echo ::{:::::::::::::::::::::::::::::}::
     sleep 5
     clear
-    echo :::::::::::::::::::::::::::::::::::::::
-    echo ::::Te_pedira_la_contraseña_5_veces::::
-    echo :::::::::::::::::::::::::::::::::::::::
-sleep 5
-clear
-#yay
-    clear
-    cd /tmp
-    mkdir install
-    cd install
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -sir --noconfirm
-    cd /tmp
-    sudo rm -d -f -r install
-#htop & fastfetch
-    clear
-    sudo pacman -Sy htop fastfetch --noconfirm
+#pacman
+    sudo pacman -Syyu flatpak htio fastfetch xfce4 xfce4-goodies xorg lightdm lightdm-gtk-greeter libreoffice-fresh-es vlc vlc-plugin-ffmpeg konsole discover ark pavucontrol spotify-launcher linux-zen okular kate networkmanager network-manager-applet bluedevil  --noconfirm
 #flatpak
-    clear
-    sudo pacman -Sy flatpak
-#instalacion del escritorio
-    clear
-    sudo pacman -Sy xfce4 xfce4-goodies xorg --noconfirm
-#lightdm
-    clear
-    sudo pacman -Sy lightdm lightdm-gtk-greeter --noconfirm
-    sudo systemctl enable lightdm
-#libreoffice
-    clear
-    echo :::::::::::::::
-    echo ::LibreOffice::
-    echo :::::::::::::::
-    sleep 2
-    sudo pacman -Sy libreoffice-fresh-es --noconfirm
-#onlyoffice
-    clear
-    echo ::::::::::::::
-    echo ::OnlyOffice::
-    echo ::::::::::::::
-    echo
-    echo ::::::::::::::::::::::::::::::
-    echo ::Te_pedira_confirmacion_¨y¨::
-    echo ::::::::::::::::::::::::::::::
-    sleep 2
+    echo ::{::::::::::::::::::::::::}::
+    echo ::__te_pedira_confirmacion__::
+    echo ::{::::::::::::::::::::::::}::
+    sleep 5
     flatpak install flathub org.onlyoffice.desktopeditors
-#kde
-    clear
-    sudo pacman -Sy okular kate --noconfirm
-#vlc
-    clear
-    sudo pacman -S vlc vlc-plugin-ffmpeg --noconfirm
-#librewolf
-    clear
-    yay -Sy librewolf-bin --noconfirm
-#konsole
-    clear
-    sudo pacman -Sy konsole --noconfirm
-#discover
-    sudo pacman -Sy discover --noconfirm
-#ark
-    clear
-    sudo pacman -Sy ark --noconfirm
-#web-apps
-    clear
-    yay -Sy webapp-manager --noconfirm
-#audio
-    sudo pacman -Sy pavucontrol --noconfirm
-#spotify
-    clear
-    sudo pacman -Sy spotify-launcher --noconfirm
-#fuentes
-    clear
-    cd
-    mkdir ~/.fonts
-    yay -Sy ttf-ms-win11-auto --noconfirm
 #kernel
-    clear
-    sudo pacman -Sy linux-zen --noconfirm
     sudo pacman -R linux --noconfirm
     sudo grub-mkconfig -o /boot/grub/grub.cfg
-#internet
-    clear
-    sudo pacman -Sy networkmanager network-manager-applet usb_modeswitch --noconfirm
-    sudo pacman -S network-manager-applet --noconfirm
+#servicios
     sudo systemctl enable NetworkManager.service
     sudo systemctl enable ModemManager.service
-#bluetooth#
-    clear
-    sudo pacman -Sy bluez bluez-utils bluedevil blueberry --noconfirm
-#limpiar
-    clear
-    sudo pacman -Sc --noconfirm
-    yay -Sc --noconfirm
-#servicios
-    clear
-    systemctl enable lightdm
-#actualizar & limpiar
-    clear
-    sudo pacman -Syyu --noconfirm
-    yay -Syyu --noconfirm
+    sudo systemctl enable lightdm
+#aur
+    cd /tmp
+    mkdir aur
+    cd aur
+    git clone https://aur.archlinux.org/librewolf-bin.git
+    cd librewolf-bin/
+    makepkg -sir
+    cd /tmp/aur
+    git clone https://aur.archlinux.org/webapp-manager-git.git
+    cd webapp-manager-git
+    makepkg -sir
+    cd /tmp/aur
+    git clone https://aur.archlinux.org/ttf-ms-win11-auto.git
+    cd ttf-ms-win11-auto
+    makepkg -sir
+    cd /tmp/aur
+#limpieza
     sudo pacman -Scc --noconfirm
-    yay -Scc --noconfirm
-#otro
-    cd
-    mkdir ~/.themes
-    mkdir ~/.icons
-    
-sleep 5
-    #fin
-    echo ::::::::::::::
-    echo ::Todo_listo::
-    echo ::::::::::::::    
-sleep 5
-#reinicio
-    clear
-    sudo reboot
+    sudo pacman -Syyu --noconfirm
+    sudo pacman -Scc --noconfirm
+    cd /tmp
+    sudo rm -d -f -r aur
+#fin
+    echo ::{::::::::::::}::
+    echo ::__todo_listo__::
+    echo ::{::::::::::::}::
+    sleep 5
+    reboot
