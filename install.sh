2c9d5fc82b316e4f9f31c3b40182f21f270992f5
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-02 21:07:44 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -21,34 +21,28 @@
     sudo pacman -S intel-media-driver libva-intel-driver libva-mesa-driver mesa vulkan-intel vulkan noveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-noveau xorg-server xorg-xinit
     sudo pacman -S xfce4 lightdm lightdm-gtk-greeter --noconfirm
     sudo pacman -S flatpak htop fastfetch libreoffice-fresh-es vlc vlc-plugin-ffmpeg konsole discover ark pavucontrol spotify-launcher linux-zen okular kate networkmanager network-manager-applet bluedevil udftools --noconfirm
-#flatpak
-    flatpak install --non-interactive flathub org.onlyoffice.desktopeditors
-    flatpak install --non-interactive flathub io.gitlab.librewolf-community
 #kernel
     sudo pacman -R linux --noconfirm
     sudo grub-mkconfig -o /boot/grub/grub.cfg
 #DM
     sudo systemctl enable lightdm.service
 #aur
+    #aur
     mkdir /tmp/aur
+    #p7zip
     cd /tmp/aur
     git clone https://aur.archlinux.org/p7zip-full-bin.git
     cd p7zip-full-bin
     makepkg
-    cd /tmp/aur/p7zip-full-bin
     sudo pacman -U p7zip-full-bin-23.01-x86_64.pkg.tar.zst
-    cd /tmp/aur
-    git clone https://aur.archlinux.org/httpdirfs-git.git
-    cd httpdirfs-git
-    makepkg -si --noconfirm --skippgpcheck
-    cd /tmp/aur
-    git clone https://aur.archlinux.org/ttf-ms-win11-auto.git
-    cd ttf-ms-win11-auto
-    makepkg -si --noconfirm --skippgpcheck
+    #webapp-manager
     cd /tmp/aur
     git clone https://aur.archlinux.org/webapp-manager-git.git
     cd webapp-manager-git
-    makepkg -si --noconfirm --skippgpcheck
+    makepkg
+    sudo pacman -U 
+    #librewolf
+    cd /tmp/aur
 #limpieza
     sudo pacman -Scc --noconfirm
     sudo pacman -Syyu --noconfirm
