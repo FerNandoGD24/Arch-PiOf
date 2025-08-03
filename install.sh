8d11c8b6006c076ef0916ababcf9ce088c511b1e
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-02 23:35:22 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -41,6 +41,7 @@
         git clone https://aur.archlinux.org/p7zip-full-bin.git
         cd p7zip-full-bin
         makepkg
+        sleep 5
         sudo pacman -U p7zip-full-bin-23.01-x86_64.pkg.tar.zst --noconfirm
     #ttf
         cd /tmp/aur
@@ -60,18 +61,21 @@
         cd /tmp/aur git clone https://aur.archlinux.org/xwayland-run-git.git
         cd xwayland-run-git
         makepkg
+        sleep 5
         sudo pacman -U xwayland-run-git.0.0.4.r5.g49b83b9-1-any.tar.zst --noconfirm
     #webapp-manager
         cd /tmp/aur
         git clone https://aur.archlinux.org/webapp-manager-git.git
         cd webapp-manager-git
         makepkg
+        sleep 5
         sudo pacman -U webapp-manager-git-1.4.2.r0.gffa3ca5-1-any.pkg.tar.zst --noconfirm
     #onlyoffice
         cd /tmp/aur
         git clone https://aur.archlinux.org/onlyoffice-bin.git
         cd onlyoffice-bin
         makepkg
+        sleep 5
         sudo pacman -U onlyoffice-bin-9.0.3-1-x86_64.pkg.tar.zst --noconfirm
 #limpieza
     sudo pacman -Rns $(pacman -Qqdt --noconfirm) --noconfirm
