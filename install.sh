004df60aad4f2c4e8208df456ad12941f352188b
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-09 01:48:38 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -15,31 +15,72 @@
     echo ::__introdusca_su_contraseña::
     echo ::{::::::::::::::::::::::::}::
 #pacman
-    sudo pacman -Syyu --noconfirm
-    sudo pacman -S --needed base-devel git --noconfirm
-    sudo pacman -S xorg-server --noconfirm
-    sudo pacman -S libxss cbindgen clang imake inetutils jq lld llvm nasm nodejs python-setuptools rust unzip wasi-compiler-rt wasi-libc++ wasi-libc++abi wasi-libc yasm zip weston xorg-xwayland --noconfirm
-    sudo pacman -S intel-media-driver libva-intel-driver libva-mesa-driver mesa vulkan-intel vulkan noveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-noveau xorg-server xorg-xinit
-    sudo pacman -S libxt startup-notification mime-types ttf-font ffmpeg --noconfirm
-    sudo pacman -S xfce4 lightdm lightdm-gtk-greeter --noconfirm
-    sudo pacman -S ttf-font --noconfirm
-    sudo pacman -S python-brautifulsoup4 python-configobj python-gobject python-pillow python-setproctitle python-tldextract xapp --noconfirm
-    sudo pacman -S flatpak htop fastfetch libreoffice-fresh-es vlc vlc-plugin-ffmpeg konsole discover ark pavucontrol spotify-launcher linux-zen okular kate networkmanager network-manager-applet bluedevil udftools --noconfirm
-    sudo pacman -S firefox --noconfirm
-    sudo pacman -S gst-plugins-ugly ttf-dejavu ttf-liberation ttf-carlito --noconfirm
-    sudo pacman -S python-beautifulsoup4 xapp python-xapp python-tldextract python-setproctitle python-pillow python-configobj python-beautifulsoup4 --noconfirm
-    sudo pacman -S go --noconfirm
-    sudo pacman -S xfce4-whiskermenu-plugin --noconfirm
-#kernel
-    sudo pacman -R linux --noconfirm
-    sudo grub-mkconfig -o /boot/grub/grub.cfg
-#DM
-    sudo systemctl enable lightdm.service
-#theme
-    mkdir ~/.themes
-    mkdir ~/.icons
-#aur
-     #ttf
+    sudo pacman -Syyu
+    #dependencias
+        sudo pacman -S -needed git base-devel  xorg-server libxss cbindgen clang imake inetutils jq lld llvm nasm nodejs python-setuptools rust unzip wasi-compiler-rt wasi-libc++ wasi-libc++abi wasi-libc yasm zip weston xorg-xwayland libxt startup-notification mime-types ttf-font ffmpeg ttf-font python-brautifulsoup4 python-configobj python-gobject python-pillow python-setproctitle python-tldextract xapp flatpak gst-plugins-ugly ttf-dejavu ttf-liberation ttf-carlito python-beautifulsoup4 xapp python-xapp python-tldextract python-setproctitle python-pillow python-configobj python-beautifulsoup4  --noconfirm
+    #drivers
+        sudo pacman -S intel-media-driver libva-intel-driver libva-mesa-driver mesa vulkan-intel vulkan noveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-noveau xorg-server xorg-xinit --noconfirm
+    #escritorio
+        sudo pacman -S lxqt --noconfirm
+    #Display manager
+        sudo pacman -S lightdm --noconfirm
+        sudo systemctl enable lightdm
+    #extras
+        sudo pacman -S htop fastfetch konsole --noconfirm
+    #multimedia
+        sudo pacman -S vlc vlc-plugin-ffmpeg spotify-launcher --noconfirm
+    #ofimatica
+        sudo pacman -S libreoffice-fresh-es okular --noconfirm
+    #utilidades
+        sudo pacman -S discover ark kate --noconfirm
+    #web
+        sudo pacman -S firefox brave-browser --noconfirm
+    #internet
+        sudo pacman -S networkmanager --noconfirm
+        sudo systemctl enable NetworkManager
+        sudo systemctl start NetworkManager
+        sudo pacman -S wpa_supplicant --noconfirm
+        sudo pacman -S networkmanager --noconfirm
+        sudo systemctl status NetworkManager
+        sudo systemctl restart NetworkManager
+    #bluetooth
+        sudo pacman -S bluez bluez-utils --noconfirm
+        sudo pacman -S blueman --noconfirm
+        sudo systemctl start bluetooth.service
+        sudo systemctl enable bluetooth.service
+        sudo systemctl status bluetooth.service
+        sudo systemctl restart bluetooth.service
+    #audio
+        sudo pacman -S pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber --noconfirm
+        sudo systemctl enable --now pipewire
+        sudo systemctl enable --now wireplumber
+        sudo systemctl --global disable pulseaudio.service pulseaudio.socket
+        sudo pacman -S pavucontrol --noconfirm
+        systemctl --user status pipewire
+        systemctl --user status wireplumber
+        systemctl --user restart pipewire
+        systemctl --user restart wireplumber
+    #bluethooth y audio
+        sudo pacman -S pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber bluez bluez-utils blueman --noconfirm
+        sudo systemctl enable --now pipewire
+        sudo systemctl enable --now wireplumber
+        sudo systemctl enable --now bluetooth.service
+        sudo pacman -S pavucontrol --noconfirm
+        systemctl --user status pipewire
+        systemctl --user status wireplumber
+        sudo systemctl status bluetooth.service
+        systemctl --user restart pipewire
+        systemctl --user restart wireplumber
+        sudo systemctl restart bluetooth.service
+#Aur
+    #yay
+        cd /tmp
+        mkdir aur
+        cd aur
+        git clone https://github.com/FerNandoGD24/yay-fork.git
+        cd yay-fork
+        makepkg
+        sudo pacman -U yay-12.5.0-1-x86_64.pkg.tar.zst --noconfirm
         cd /tmp/aur
         mkdir ttf
         cd ttf
@@ -53,25 +94,28 @@
         tar -xvf ttf_u.tar -C /usr/share/fonts
         tar -xvf ttf_u.tar -C /usr/local/share/fonts
         tar -xvf ttf_u.tar -C ~/.fonts
-    #aur
-        mkdir /tmp/aur
-        cd /tmp/aur
-        git clone https://github.com/FerNandoGD24/yay-fork.git
-        cd yay-fork
-        makepkg
-        sudo pacman -U yay-12.5.0-1-x86_64.pkg.tar.zst --noconfirm
-    #yay
-        yay -Sy onlyoffice-bin webapp-manager-git --noconfirm
+#yay
+    yay -Syyuu --noconfirm
+    #ofimatica
+        yay -S onlyoffice-bin --noconfirm
+    #internet
+        yay -S webapp-manager-git --noconfirm
+#kernel
+    sudo pacman -S linux-zen --noconfirm
+    sudo pacman -R linux --noconfirm
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+#extras
+    cd
+    mkdir .themes
+    mkdir .icons
 #limpieza
     sudo pacman -Rns $(pacman -Qqdt --noconfirm) --noconfirm
-    sudo pacman -Scc --noconfirm
     sudo pacman -Syyu --noconfirm
     sudo pacman -Scc --noconfirm
-    yay -Syyu --noconfirm
     yay -Rns $(yay -Qpdt --noconfirm) --noconfirm
+    yay -Syyu --noconfirm
     yay -Scc --noconfirm
-    cd /tmp
-    sudo rm -d -f -r aur
+    sudo rm -dfr /tmp/aur
 #fin
     echo ::{::::::::::::}::
     echo ::__todo_listo__::
@@ -90,3 +134,4 @@
     echo apagando
     sleep 1
     sudo shutdown -h now
+    
